fceead5bd6021300cb2af4b85d547afa48b4a038
LOCKETUNIT_PC02\LocketGoma <nothappa@naver.com>
2017-03-16 13:03:46 +0900

    틀

---
+++ TTT_main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "TTT_Head.h"
+
+using namespace std;
+
+class TTT_Engine{
+	private ::
+	
+	public ::
+	int level=1; // 레벨링
+	bool isFirst=TURE; // 선,후공
+	
+	bool selectFirst(); // 선후공 선택
+};
+
