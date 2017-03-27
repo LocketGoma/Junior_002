30ca2e8b368ae9d39975feb5639c2f3030741852
LocketGoma <nothappa@naver.com>
2017-03-28 00:24:09 +0900

    명세 : 예시코드 업로드

--- TTT_main.cpp
+++ TTT_main.cpp
@@ -7,12 +7,16 @@ class TTT_Engine{
 	private :
 	int ground[3][3];	// '게임판' (0 - 없음 / 1 - X / 2 - O)
 	bool isInput[3][3]; // ground와 같이 돔 / 뭔가 있나 없나 판단 (true = 비어있음 / false = 차 있음)
+	int place[9][2]; // 넣을수 있는 자리의 가지 수. <- 최대 9개.
 
 	int level = 3; // 난이도. 기본 3 (Ahead 수행 회수)
 
 	void initall(); // 초기화 함수
+	void init_place(); // 가지 수 초기화 함수.
+	int place_trace(); // place에 빈 칸 찾아주는 함수.
+
 	void Evaluate(); // Evaluate (최적화) 함수
-	void Ahead(); // '내다보기' 함수
+	void Ahead(int level); // '내다보기' 함수
 	void input_pin(int x,int y); // '핀 올리기'
 	//void isWin(int pin); // 결과 판독 <- 원래는 private에 가 있을거임.
 
@@ -72,12 +76,45 @@ void TTT_Engine::isWin(int pin) {
 		}
 	}
 }
+void TTT_Engine::Ahead(int level) { // 1. 남은 칸이 몇개인가. 2. 가능성이 있는 자리가 있는가. 3. 상대방은 어떠한가. 4. 2~3을 여러번 반복하면 좀 더 달라지는가.
+	int x_axis;
+	int y_axis;
+	int blank; // '빈 칸 개수'
+	blank=place_trace();  // 1번.
+	for (int i = 0; i < level; i++) {
+
+	}
+
+	input_pin(x_axis + 1, y_axis + 1);
+
+}
+int TTT_Engine::place_trace() {
+	int count = 0;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (isInput[i][j] == true) {
+				place[count][0] = i;
+				place[count][1] = j;
+				count++;
+			}
+		}
+	}
+	return count + 1;
+}
+//int * 
 void TTT_Engine::initall() {
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
 			ground[i][j] = 0;
 			isInput[i][j] = true;
 		}
+	}
+	init_place();
+}
+void TTT_Engine::init_place() {
+	for (int i = 0; i < 0; i++) {
+		place[i][0] = -1;
+		place[i][1] = -1;
 	}
 }
 void TTT_Engine::input_select() {
